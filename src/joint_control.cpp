117a619008b361fdf8bdb8c8ad623a1abc4f73ba
Thomas G <thomas.godden@outlook.com>
2023-02-27 17:14:37 +0000

    Work on cpp joint controller

--- src/joint_control.cpp
+++ src/joint_control.cpp
@@ -4,11 +4,25 @@
 #include "std_msgs/msg/float32_multi_array.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
 
+using std::placeholders::_1;
+
+
 class JointController : public rclcpp::Node
 {
     public:
+
+    rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr position_publisher;
+    rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr torque_publisher;
+
+    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_publisher;
+
     JointController() : Node("joint_controller")
     {
-        RCLCPP_INFO(this->get_logger(), "Starting SLIDER joint control")
+        RCLCPP_INFO(this->get_logger(), "Starting SLIDER joint control");
+
+        joint_state_publisher = this->create_publisher<sensor_msgs::msg::JointState>(
+            "/joint_states",
+            10
+        )
     }
 }
